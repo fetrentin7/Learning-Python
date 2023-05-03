54d4c51e826278647b52a2c61bdf93a76602a470
Fernando <fetrentin7@gmail.com>
2023-05-03 14:08:02 -0300

    aa

---
+++ main.py
@@ -0,0 +1,12 @@
+
+import matplotlib as mpl
+import csv
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('circuits.csv')
+df_circuitRef = df[(df['circuitRef'] =='albert_park') & (df['name'] == 'Australian Grand Prix')]
+
+
+print(df.circuitRef.unique())
+print(df_circuitRef)
