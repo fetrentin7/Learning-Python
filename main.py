b4a22140bfa3d4f649d352bdf8a605b18880d06f
Fernando <fetrentin7@gmail.com>
2023-05-03 17:55:37 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import pandas as pd
 
 df = pd.read_csv(r'C:\TrabalhoPython\pythonProject1\f12022\f1_drivers_points (1).csv')
 
-print(df)
+# printa coluna com header [[]]
+print(df[['driver']].to_string(index=False))
+
+data_driver = df.loc[(df['nationality'] == 'GER')]
+print(data_driver)
+
