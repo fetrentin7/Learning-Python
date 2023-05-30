7022108a40fea1dad4da1d30e44461cd82b0d652
fetrentin7 <fetrentin7@gmail.com>
2023-05-30 15:10:05 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	degrees/degrees.py
    Merge remote-tracking branch 'origin/master'
    
    # Conflicts:
    #	main.py

--- Pygame/main.py
+++ Pygame/main.py
@@ -12,23 +12,23 @@ pygame.display.set_caption("Game")
 WIDTH, HEIGHT = 1280, 720
 FPS = 60
 PLAYER_SPEED = 5
-
 main_window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 def flip_image(sprites):
-    return [pygame.transform.flip(sprites, True, False) for sprite in sprites]
+    return [pygame.transform.flip(sprite, True, False)
+            for sprite in sprites]
 
 
-def load_sprite_sheets(dir1, dir2, width, height, direction=False):
-    path = join('assets', dir1, dir2)
+def load_sprite_sheets(dir1, dir2, width, height, move_check=False):
+    path = join("assets", dir1, dir2)
     images = [f for f in listdir(path) if isfile(join(path, f))]  # load files from `assets` and split them images
-    # into individual
+    # into individual ones
 
     all_sprites = {}
 
-    for img in images:
-        sprite_sheet = pygame.image.load(join(path, img)).convert_alpha()  # load image and append path
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
 
         sprites = []
         for i in range(sprite_sheet.get_width() // width):
@@ -37,25 +37,29 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
             surface.blit(sprite_sheet, (0, 0), rect)
             sprites.append(pygame.transform.scale2x(surface))
 
-        if direction:
-            flipped_sprites = [flip(sprite, True, False) for sprite in sprites]
-            all_sprites[img.replace('.png', '') + '_right'] = sprites
-            all_sprites[img.replace('.png', '') + '_left'] = flipped_sprites
+        if move_check:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip_image(sprites)
         else:
-            all_sprites[img.replace('.png', '')] = sprites
+            all_sprites[image.replace(".png", "")] = sprites
+
     return all_sprites
 
 
 class Player(pygame.sprite.Sprite):
     PLAYER_COLOR = (255, 0, 0)
     GRAVITY_ACC = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "VirtualGuy", 32, 32, True)
+    ANIMATION_DELAY = 5
 
     def __init__(self, x, y, width, height):
+        super().__init__()
+
         self.rect = pygame.Rect(x, y, width, height)  # add colision and move player around
         self.x_vel = 0
         self.y_vel = 0
         self.mask = None
-        self.move_check = 'Left'
+        self.move_check = "left"
         self.animation_counter = 0
         self.fall_counter = 0
 
@@ -65,28 +69,40 @@ class Player(pygame.sprite.Sprite):
 
     def player_move_right(self, velocity):
         self.x_vel = +velocity
-        if self.move_check != 'Right':
-            self.move_check = 'Right'
+        if self.move_check != "right":
+            self.move_check = "right"
             self.animation_counter = 0
 
     def player_move_left(self, velocity):
         self.x_vel = -velocity
-        if self.move_check != 'Left':
-            self.move_check = 'Left'
+        if self.move_check != "left":
+            self.move_check = "left"
             self.animation_counter = 0
 
     def move_lef_right(self, fps):  # fps variable will increase the y.vel by gravity
-        self.y_vel += min(1, (self.fall_counter / fps) * self.GRAVITY_ACC)
+        # self.y_vel += min(1, (self.fall_counter / fps) * self.GRAVITY_ACC)
         self.player_move(self.x_vel, self.y_vel)
 
         self.fall_counter = self.fall_counter + 1
+        self.update_sprite()
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.move_check
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprites_index = self.animation_counter // self.ANIMATION_DELAY % len(sprites)
+        self.sprite = sprites[sprites_index]
+        self.animation_counter += 1
 
     def draw(self, win):
-        pygame.draw.rect(win, self.PLAYER_COLOR, self.rect)
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 def get_background(name):
-    image = pygame.image.load(join('assets', 'Background', name))
+    image = pygame.image.load(join("assets", "Background", name))
     x, y, width, height = image.get_rect()
     tiles = []
 
